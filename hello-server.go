ed78c668cdbf712aaa45cfd66bbdb99699998e14
Cora Iberkleid <ciberkleid@gmail.com>
2020-06-09 19:43:59 -0400

    initial commit

---
+++ hello-server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// HTTP Handler - Hello, world
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello, world!\n")
+}
+
+// HTTP Handler - Echo headers into response body
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":8080", nil)
+}
