953647ade37954e10d01d2a85f1a2012036b5dfd
Cora Iberkleid <ciberkleid@gmail.com>
2020-06-10 17:11:41 -0400

    simpler code

--- hello-server.go
+++ hello-server.go
@@ -2,26 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-// HTTP Handler - Hello, sunshine
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Hello, sunshine!\n")
-}
-
-// HTTP Handler - Echo headers into response body
-func headers(w http.ResponseWriter, req *http.Request) {
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
-}
-
 func main() {
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/headers", headers)
-
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, world!\n")
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
